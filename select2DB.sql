cb89b25d928ea4152908674de9c72a3d9ad4bbd4
LebedeffJr <Lebedeffjr@gmail.com>
2023-05-06 19:18:53 +0300

    advanced select2 HW

--- select2DB.sql
+++ select2DB.sql
@@ -3,19 +3,20 @@ JOIN genreartist ga ON ga.genre_id = g.id
 JOIN artist a ON a.id = ga.artist_id
 GROUP BY g.title;
 
-SELECT a.title, count(t.title) FROM album a 
-JOIN track t ON t.album_id = a.id 
-WHERE a.release_year BETWEEN 2019 AND 2020
-GROUP BY a.title;
+SELECT count(t.title) FROM track t 
+JOIN album a ON t.album_id = a.id 
+WHERE a.release_year BETWEEN 2019 AND 2020;
 
 SELECT a.title, AVG(t.duration) FROM album a 
 JOIN track t ON t.album_id = a.id 
 GROUP BY a.title ;
 
 SELECT a.name FROM artist a
-JOIN albumartist aa ON a.id = aa.artist_id 
-JOIN album a2 ON a2.id = aa.album_id 
-WHERE 2020 NOT IN (a2.release_year);
+WHERE a.name NOT IN (
+	SELECT a2.name FROM artist a2
+	JOIN albumartist aa ON a2.id = aa.artist_id 
+	JOIN album a3 ON a3.id = aa.album_id 
+	WHERE a3.release_year = 2020);
 
 SELECT c.title FROM collection c 
 JOIN trackcollection tc ON tc.collection_id = c.id 
@@ -25,14 +26,11 @@ JOIN albumartist aa ON aa.album_id = a.id
 JOIN artist ar ON aa.artist_id = ar.id
 WHERE 'Nirvana' IN (ar.name);
 
-SELECT a.title FROM album a
+SELECT DISTINCT a.title FROM album a
 JOIN albumartist aa ON aa.album_id = a.id 
-JOIN artist ar ON ar.id = aa.artist_id 
-JOIN genreartist ga ON ga.artist_id = ar.id 
-JOIN genre g ON g.id = ga.genre_id
-GROUP BY a.title 
-ORDER BY count(ga.genre_id) DESC
-LIMIT 1;
+JOIN genreartist ga ON ga.artist_id = aa.artist_id 
+GROUP BY a.id, ga.artist_id 
+HAVING count(ga.genre_id) > 1;
 
 SELECT t.title FROM track t 
 LEFT JOIN trackcollection tc ON tc.track_id = t.id 
@@ -49,8 +47,10 @@ WHERE id = 22;
 
 SELECT a.title FROM album a 
 JOIN track t ON t.album_id = a.id 
-GROUP BY a.title 
-ORDER BY count(t.album_id) 
-LIMIT 1
-;
+GROUP BY a.id  
+HAVING count(t.id) = (
+	SELECT count(t2.id) FROM track t2
+	GROUP BY t2.album_id 
+	ORDER BY 1
+	LIMIT 1);
 
